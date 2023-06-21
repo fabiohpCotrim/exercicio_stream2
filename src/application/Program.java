56ee2b4ede0d2a6e8c7bba38e2eefa4a6daa3b7f
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-06-21 20:30:26 -0300

    Pipeline for emails

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Employee;
 
@@ -16,29 +18,37 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Enter full file path: ");
-		String path = sc.nextLine(); 
-		
+		String path = sc.nextLine();
+
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
+
 			List<Employee> list = new ArrayList<>();
-			
+
 			String line = br.readLine();
 			while (line != null) {
 				String[] fields = line.split(",");
 				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
 				line = br.readLine();
 			}
-			
+
+			System.out.print("Enter salary: ");
+			double salary = sc.nextDouble();
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
+
+			Comparator<String> comp = (s1, s2) -> s1.compareTo(s2);
+
+			List<String> emails = list.stream().filter(emp -> emp.getSalary() > salary).map(emp -> emp.getEmail())
+					.sorted(comp).collect(Collectors.toList());
+
+			emails.forEach(System.out::println);
+
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-			
+
 		}
-		
-		
-		
-		
+
 		sc.close();
 
 	}
